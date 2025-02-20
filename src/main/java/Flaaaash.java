97802b21f8312109d976c9b437caa2618ada96b2
Liu Xuyan <1739606183@qq.com>
2025-02-20 23:11:35 +0800

    Add deleteTask(). Change tasks array to a ArrayList type for dynamic sizes.

--- src/main/java/Flaaaash.java
+++ src/main/java/Flaaaash.java
@@ -1,53 +1,62 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Flaaaash {
-    private static final int maxCount = 100;
-    private static final Task[] tasks = new Task[maxCount];
-    private static int taskCount = 0;
+//    private static final int maxCount = 100;
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+//    private static int taskCount = 0;
 
     private static void markTask(int index) {
         System.out.println("____________________________________________________________");
-        if (index < 0 || index > taskCount) {
+        if (index < 0 || index > tasks.size()) {
             System.out.println(" Invalid index");
         } else {
             System.out.println(" Nice! I've marked this task as done:");
-            tasks[index - 1].markAsDone();
-            System.out.println("    " + tasks[index - 1]);
+            tasks.get(index - 1).markAsDone();
+            System.out.println("    " + tasks.get(index - 1));
         }
         System.out.println("____________________________________________________________");
     }
 
     private static void unmarkTask(int index) {
         System.out.println("____________________________________________________________");
-        if (index < 0 || index > taskCount) {
+        if (index < 0 || index > tasks.size()) {
             System.out.println(" Invalid index");
         } else {
             System.out.println(" OK, I've marked this task as not done yet:");
-            tasks[index - 1].unmarkAsDone();
-            System.out.println("    " + tasks[index - 1]);
+            tasks.get(index - 1).unmarkAsDone();
+            System.out.println("    " + tasks.get(index - 1));
         }
         System.out.println("____________________________________________________________");
     }
 
     private static void addTask(Task task) {
-        tasks[taskCount] = task;
-        taskCount += 1;
+        tasks.add(task);
 
         System.out.println("____________________________________________________________");
         System.out.println(" Got it. I've added this task:");
-        System.out.println("    " + tasks[taskCount - 1]);
-        System.out.println(" Now you have " + taskCount + " tasks in the list.");
+        System.out.println("    " + task);
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void deleteTask(int index) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("  " + tasks.get(index - 1));
+        tasks.remove(tasks.get(index - 1));
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
         System.out.println("____________________________________________________________");
     }
 
     private static void list() {
         System.out.println("____________________________________________________________");
-        if (taskCount == 0) {
+        if (tasks.isEmpty()) {
             System.out.println(" List is empty!");
         } else {
             System.out.println(" Here are the tasks in your list:");
-            for(int i = 0; i < taskCount; i += 1) {
-                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            for(int i = 0; i < tasks.size(); i += 1) {
+                System.out.println(" " + (i + 1) + ". " + tasks.get(i));
             }
         }
         System.out.println("____________________________________________________________");
@@ -56,6 +65,12 @@ public class Flaaaash {
     private static void repeat(String input) {
         System.out.println("____________________________________________________________");
         System.out.println(" " + input);
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void reportUnknownCommand(String input) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" <" + input + "> is an unknown command. Please Refer to the user guide." );
         System.out.println("____________________________________________________________");
     }
 
@@ -101,13 +116,22 @@ public class Flaaaash {
                 break;
 
             case "unmark":
-            try {
-                unmarkTask(Integer.parseInt(inputParts[1]));
+                try {
+                    unmarkTask(Integer.parseInt(inputParts[1]));
+                    break;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid format! Use: unmark <Task index>");
+                }
                 break;
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Invalid format! Use: unmark <Task index>");
-            }
-            break;
+
+            case "delete":
+                try {
+                    deleteTask(Integer.parseInt(inputParts[1]));
+                    break;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid format! Use: delete <Task index>");
+                }
+                break;
 
             case "todo":
                 if (inputParts.length < 2) {
@@ -144,7 +168,7 @@ public class Flaaaash {
             break;
 
             default:
-                repeat(command);
+                reportUnknownCommand(command);
             }
         }
     }
